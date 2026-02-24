6556cde47797610aa8e39eef01d70cb4a6cc02b1
Cameron Deputy <cameroncdeputy@gmail.com>
2026-02-24 14:08:04 -0800

    adding goog analytics

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col bg-white">
+        <GoogleAnalytics />
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
